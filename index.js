b3af1452a4c3ab34acf71199ab42d02e7679dfbc
Osvaldo Escobar <eoeg1410@gmail.com>
2023-11-10 22:31:35 +0100

    add new date

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 // convert this into commonjs
 import { format } from 'date-fns';
 
-
-console.log(format(new Date(), 'dd-MM-yyyy'));
+console.log(format(new Date(), 'dd-MM-yyyy HH:mm:ss'))
+console.log('new Date', format(new Date(), 'dd-MM-yyyy HH:mm:ss'))
